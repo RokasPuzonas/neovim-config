b82109f8b29c0e05c4061198b22f77649309fead
Rokas Puzonas <rokas.puz@gmail.com>
2021-11-01 15:03:03 +0200

    fix: add command to restart, recompile and sync plugins at the same time

--- lua/config/reload.lua
+++ lua/config/reload.lua
@@ -2,3 +2,5 @@ local map = require 'utils.map'
 
 map('n', '<leader>rc', ':Restart<cr>', {silent = true})
 
+vim.cmd [[command! PackerRestart exe 'Restart' | exe 'PackerCompile' | exe 'PackerSync']]
+
