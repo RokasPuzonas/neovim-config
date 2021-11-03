5e7f759d7f1b5952d8027043968b24fe92277fd4
Rokas Puzonas <rokas.puz@gmail.com>
2021-11-04 01:21:36 +0200

    fix: better movement between tabs

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -21,15 +21,7 @@ map('n', '<M-l>', ':vertical resize +2<cr>', silent)
 map('v', '<', '<gv')
 map('v', '>', '>gv')
 
--- Go to tab by number
-map('n', '<leader>1', '1gt')
-map('n','<leader>2', '2gt')
-map('n','<leader>3', '3gt')
-map('n','<leader>4', '4gt')
-map('n','<leader>5', '5gt')
-map('n','<leader>6', '6gt')
-map('n','<leader>7', '7gt')
-map('n','<leader>8', '8gt')
-map('n','<leader>9', '9gt')
-map('n','<leader>0', ':tablast<cr>')
+-- Better movement between tabs
+map('n', '<C-Left>', ':tabprevious<cr>', silent)
+map('n', '<C-Right>', ':tabnext<cr>', silent)
 
