a6476a94fb57acd940846b6ecae2a4520f6ac6ae
Rokas Puzonas <rokas.puz@gmail.com>
2022-04-01 22:20:55 +0300

    fix: update commenting plugin for vue

--- lua/config/comment.lua
+++ lua/config/comment.lua
@@ -2,7 +2,9 @@ local config = {}
 
 if packer_plugins['nvim-ts-context-commentstring'] and packer_plugins['nvim-ts-context-commentstring'].loaded then
 	config.hook = function()
-		require("ts_context_commentstring.internal").update_commentstring()
+		if vim.api.nvim_buf_get_option(0, "filetype") == "vue" then
+      require("ts_context_commentstring.internal").update_commentstring()
+    end
 	end
 end
 
