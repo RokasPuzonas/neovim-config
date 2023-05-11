9d088fe9d5d2e5875943478c6747f78f16cfe14b
Rokas Puzonas <rokas.puz@gmail.com>
2023-05-11 17:31:02 +0300

    disable ufo

--- lua/config/ufo.lua
+++ lua/config/ufo.lua
@@ -2,9 +2,12 @@ return {
 	"kevinhwang91/nvim-ufo",
 	requires = {"kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter"},
 	config = function ()
+		local default = {"treesitter", "indent"}
+		local filetypes = { c = {'indent'} }
+
 		require("ufo").setup({
 			provider_selector = function(bufnr, filetype, buftype)
-				return {"treesitter", "indent"}
+				return filetypes[filetype] or default
 			end
 		})
 		vim.api.nvim_set_hl(0, "UfoFoldedBg", { link = "ColorColumn" })
