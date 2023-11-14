a3a12b100e65b20a0f586df50c899f64899b8913
Rokas Puzonas <rokas.puz@gmail.com>
2023-11-14 15:10:31 +0200

    update tmux navigator keybinds

--- init.lua
+++ init.lua
@@ -613,10 +613,11 @@ do
   vim.keymap.set("n", "Q", "<nop>")
 
   -- Window movement
-  vim.keymap.set("n", "<c-h>", "<c-w>h")
-  vim.keymap.set("n", "<c-j>", "<c-w>j")
-  vim.keymap.set("n", "<c-k>", "<c-w>k")
-  vim.keymap.set("n", "<c-l>", "<c-w>l")
+  vim.g.tmux_navigator_no_mappings = 1
+  vim.keymap.set("n", "<c-h>", ":TmuxNavigateLeft<CR>", { noremap = true, silent=true })
+  vim.keymap.set("n", "<c-j>", ":TmuxNavigateDown<CR>", { noremap = true, silent=true })
+  vim.keymap.set("n", "<c-k>", ":TmuxNavigateUp<CR>", { noremap = true, silent=true })
+  vim.keymap.set("n", "<c-l>", ":TmuxNavigateRight<CR>", { noremap = true, silent=true })
 
   -- Resize windows
   vim.keymap.set('n', '<M-j>', ':resize -2<cr>', { silent = true })
